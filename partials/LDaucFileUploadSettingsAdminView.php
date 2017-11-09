77700cff1fc8357d7d7f78749bb3ec2e8a36da33
brbaso <brbaso@gmail.com>
2017-11-09 17:03:30 +0100

    code cleaning

--- partials/LDaucFileUploadSettingsAdminView.php
+++ partials/LDaucFileUploadSettingsAdminView.php
@@ -1,39 +1,39 @@
 <?php
 // Exit if accessed directly
-if (!defined('ABSPATH'))
-    exit;
+if ( !defined( 'ABSPATH' ) )
+	exit;
 
 /**
  * File Upload options view
  */
-class LDaucFileUploadSettingsAdminView{
+class LDaucFileUploadSettingsAdminView {
 
 	private $functions;
 
 	/**
 	 * Initialize the class.
 	 *
-	 * @param      object    $functions      Object.
+	 * @param      object $functions Object.
 	 */
-	public function __construct( $functions ) {
+	public function __construct ( $functions ) {
 		$this -> functions = $functions;
-	}	
-	
+	}
+
 	/**
-	 * Render the page.	 
+	 * Render the page.
 	 */
-	public function display(){		
+	public function display () {
 		?>
-		<div class="wrap">
-			<div class="wrap-left">
-			<h1> <?php echo __( 'LearnDash Assignment Uploads Control Settins', 'ldauc' ); ?> </h1>
-			<form action="options.php" method="POST">
-				<?php settings_fields( 'ld_file_upload_settings' ); ?>
-				<?php do_settings_sections( 'learndash-lms/file-upload-settings' ); ?>
-				<?php submit_button(); ?>
-			</form>
-			</div>
-		</div>					
-	<?php
+        <div class="wrap">
+            <div class="wrap-left">
+                <h1> <?php echo __( 'LearnDash Assignment Uploads Control Settins', 'ldauc' ); ?> </h1>
+                <form action="options.php" method="POST">
+					<?php settings_fields( 'ld_file_upload_settings' ); ?>
+					<?php do_settings_sections( 'learndash-lms/file-upload-settings' ); ?>
+					<?php submit_button(); ?>
+                </form>
+            </div>
+        </div>
+		<?php
 	}
 }
