fb650c0c5a73906c631bce0858cbc2b49c552b4b
brbaso <brbaso@gmail.com>
2017-11-09 17:06:47 +0100

    code cleaning

--- helpers/GetTemplateHelper.php
+++ helpers/GetTemplateHelper.php
@@ -6,7 +6,7 @@ if ( !defined( 'ABSPATH' ) )
 /**
  * Get template class
  */
-class GetTemplateHelper{
+class GetTemplateHelper {
 
 	private $plugin_dir_name = '';
 	private $plugin_dir = '';
@@ -18,7 +18,7 @@ class GetTemplateHelper{
 	 * @param $plugin_dir
 	 * @param $plugin_url
 	 */
-	public function __construct( $plugin_dir_name, $plugin_dir, $plugin_url ) {
+	public function __construct ( $plugin_dir_name, $plugin_dir, $plugin_url ) {
 		$this -> plugin_dir_name = $plugin_dir_name;
 		$this -> plugin_dir = $plugin_dir;
 		$this -> plugin_url = $plugin_url;
@@ -38,73 +38,59 @@ class GetTemplateHelper{
 	 * @return mixed
 	 *
 	 */
-	public function get_template( $name, $args, $echo = false, $return_file_path = false ) {
+	public function get_template ( $name, $args, $echo = false, $return_file_path = false ) {
 
 		$template_paths = array();
 		$file_pathinfo = pathinfo( $name );
 
-		if ( !isset( $file_pathinfo['extension'] ) ) {
-			$file_pathinfo['extension'] = 'php';
+		if ( !isset( $file_pathinfo[ 'extension' ] ) ) {
+			$file_pathinfo[ 'extension' ] = 'php';
 		}
 
 		// If for some reason the $name has an extension we reset the $name to be without it.
-		if ( $file_pathinfo['extension'] == 'php' ) {
-			$template_paths = array_merge(
-				$template_paths,
-				array(
-					$this -> plugin_dir_name. '/' . $file_pathinfo['filename'] .'.'. $file_pathinfo['extension'],
-					$file_pathinfo['filename'] .'.'. $file_pathinfo['extension']
-				)
-			);
+		if ( $file_pathinfo[ 'extension' ] == 'php' ) {
+			$template_paths = array_merge( $template_paths, array( $this -> plugin_dir_name . '/' . $file_pathinfo[ 'filename' ] . '.' . $file_pathinfo[ 'extension' ], $file_pathinfo[ 'filename' ] . '.' . $file_pathinfo[ 'extension' ] ) );
 
-		// and if the extension is .js or .css we check for minified versions.
-		} else if ( ( $file_pathinfo['extension'] == 'js' ) || ( $file_pathinfo['extension'] == 'css' ) ) {
+			// and if the extension is .js or .css we check for minified versions.
+		} else if ( ( $file_pathinfo[ 'extension' ] == 'js' ) || ( $file_pathinfo[ 'extension' ] == 'css' ) ) {
 
 			// force here returning file path
-			$return_file_path = true ;
-			$template_paths = array_merge(
-				$template_paths,
-				array(
-					$this -> plugin_dir_name. '/' . $file_pathinfo['filename'] .'.'. $file_pathinfo['extension'],
-					$this -> plugin_dir_name. '/' . $file_pathinfo['filename'] .'.min.'. $file_pathinfo['extension'],
-					$file_pathinfo['filename'] .'.'. $file_pathinfo['extension'],
-					$file_pathinfo['filename'] .'.min.'. $file_pathinfo['extension']
-				)
-			);
+			$return_file_path = true;
+			$template_paths = array_merge( $template_paths, array( $this -> plugin_dir_name . '/' . $file_pathinfo[ 'filename' ] . '.' . $file_pathinfo[ 'extension' ], $this -> plugin_dir_name . '/' . $file_pathinfo[ 'filename' ] . '.min.' . $file_pathinfo[ 'extension' ], $file_pathinfo[ 'filename' ] . '.' . $file_pathinfo[ 'extension' ], $file_pathinfo[ 'filename' ] . '.min.' . $file_pathinfo[ 'extension' ] ) );
 
 		}
 		$filepath = locate_template( $template_paths );
 
 		// locate_template found them there but if we have js or css we need URL instead of PATH
-		if ( ( $file_pathinfo['extension'] == 'js' ) || ( $file_pathinfo['extension'] == 'css' ) ) {
+		if ( ( $file_pathinfo[ 'extension' ] == 'js' ) || ( $file_pathinfo[ 'extension' ] == 'css' ) ) {
 			$filepath = str_replace( ABSPATH, '/', $filepath );
 		}
 
-		if ( ! $filepath ) {
+		if ( !$filepath ) {
 			$template_files = array();
 
-			foreach( $template_paths as $template_path ) {
+			foreach ( $template_paths as $template_path ) {
 				$template_file = basename( $template_path );
 				if ( !array_key_exists( $template_file, $template_files ) ) {
-					$template_files[$template_file] = $template_file;
+					$template_files[ $template_file ] = $template_file;
 				}
 			}
 
 			if ( !empty( $template_files ) ) {
-				foreach( $template_files as $template_file ) {
+				foreach ( $template_files as $template_file ) {
 					if ( file_exists( $this -> plugin_dir . 'templates/' . $template_file ) ) {
 						$filepath = $this -> plugin_dir . 'templates/' . $template_file;
 						break;
 					}
 				}
 
-				if ( ( $file_pathinfo['extension'] == 'js' ) || ( $file_pathinfo['extension'] == 'css' ) ) {
-					$filepath = untrailingslashit($this -> plugin_url).str_replace( $this -> plugin_dir, '/', $filepath );
+				if ( ( $file_pathinfo[ 'extension' ] == 'js' ) || ( $file_pathinfo[ 'extension' ] == 'css' ) ) {
+					$filepath = untrailingslashit( $this -> plugin_url ) . str_replace( $this -> plugin_dir, '/', $filepath );
 				}
 			}
 		}
 
-		if ( ! $filepath ) {
+		if ( !$filepath ) {
 			return false;
 		}
 
@@ -112,7 +98,7 @@ class GetTemplateHelper{
 		 * Filter filepath for template being called
 		 *
 		 */
-		$filepath = apply_filters( $this -> plugin_dir_name.'_template', $filepath, $name, $args, $echo, $return_file_path );
+		$filepath = apply_filters( $this -> plugin_dir_name . '_template', $filepath, $name, $args, $echo, $return_file_path );
 
 		if ( $return_file_path ) {
 			return $filepath;
@@ -125,7 +111,7 @@ class GetTemplateHelper{
 			include( $filepath );
 			$contents = ob_get_clean();
 
-			if ( ! $echo ) {
+			if ( !$echo ) {
 				return $contents;
 			}
 			echo $contents;
